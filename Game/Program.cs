812649b64dd56c44eb948b9f78b1ef72afb88dea
LeeLuiza <116955531+LeeLuiza@users.noreply.github.com>
2023-02-18 12:47:29 +0200

    Game

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,23 +1,76 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Data;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Claims;
 using static System.Net.Mime.MediaTypeNames;
-
-class Programm
+class Program
 {
     public static void Main()
     {
-        int counterror = 3;
-        int error = 0;
+        Conditions game = new Conditions();
+    }
+}
 
-        Console.WriteLine("Введите число от 1 до 8");
-        int number = (int.Parse(Console.ReadLine()) - 1);
-        string word = number.ReadWord();
+public static class Word //считываем рандомное слово
+{
+    public static string ReadWord(this int number)
+    {
+        int i = 1;
+        var random = new Random();
+        
+        List<string> text = new List<string>();
+
+        using (StreamReader fs = new StreamReader(@"Word.txt"))
+        {
+            while (true)
+            {
+                string temp = fs.ReadLine();
+                if (temp is null) break;
+                text.Add(temp);
+                i++;
+            }
+        }
+        int index = random.Next(text.Count);
+        int a = text[index].Length;
+        Console.WriteLine($"Количество букв в слове {a}");
+
+        //Console.WriteLine(text[index]);
+        return text[index];
+    }
+
+    public static string CheckCharacter(this string word, char letter)
+    {
+        int m = 0;
+        char[] array;
+        array = word.ToCharArray();
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (array[i] == letter)
+            {
+                word = word.Remove(i, 1);              
+                break;
+            }
+        }
+        return word;
+    }
+}
+
+
+public class Conditions 
+{
+    public string word = 1.ReadWord();
+
+    public Conditions()
+    {
+        int counterror = 4;
+        int error = 0;
 
         string w = word;
         int lengthword = word.Length;
 
-        for (int i = 0; i < word.Length; i++)
+        while (true)
         {
             Console.WriteLine("Введите букву");
             char latter = char.Parse(Console.ReadLine());
@@ -25,12 +78,14 @@ class Programm
             w = w.CheckCharacter(latter);
             if (w.Length == lengthword)
             {
-                error += 1;
+                error++;
                 Console.WriteLine("Этой буквы нет в слове. " + $"Кол-во оставшихся ошибок - {counterror - error}");
+                //Console.WriteLine($"Кол-во ошибок {error}");
+                error.Sketch();
             }
             else
             {
-                lengthword -= 1;
+                lengthword--;
                 Console.WriteLine("Эта буква  есть в слове");
             }
 
@@ -39,50 +94,37 @@ class Programm
                 Console.WriteLine($"Игра окончена. Вы проиграли. Загаданное слово {word}");
                 break;
             }
-            
-            if (lengthword == 0) Console.WriteLine($"Вы выиграли. Загаданное слово {word}");          
-        }
-    }
 
-}
-
-public static class Reading //считываем радномное слово
-{
-    public static string ReadWord(this int number)
-    {
-        int i = 0;
-        string[] text;
-        text = new string[8];
-        using (StreamReader fs = new StreamReader(@"Word.txt"))
-        {
-            while (true)
-            {             
-                string temp = fs.ReadLine();                
-                if (temp == null) break;              
-                text[i] += temp;
-                i += 1;
-            }
-        }
-        return text[number];
-    }
-    
-}
-
-public static class check
-{
-    public static string CheckCharacter(this string word, char latter)
-    {
-        int m = 0;  
-        char[] array;
-        array = word.ToCharArray();
-        for (int i = 0; i < word.Length; i++)
-        {
-            if (array[i] == latter)
+            if (lengthword == 0)
             {
-                word = word.Remove(i, 1);
+                Console.WriteLine($"Вы выиграли. Загаданное слово {word}");
                 break;
             }
-        }     
-        return word;
+        }
     }
 }
+
+public static class Draw
+{
+    public static void Sketch(this int error)
+    {
+        if (error == 1)
+            Console.WriteLine("           ________\r\n");
+        if (error == 2)
+            Console.WriteLine("           ________\r\n          |       \r\n          |        \r\n          |        \r\n          |          \r\n          |       \r\n          |        \r\n          |          \r\n          |\r\n          |");
+        if (error == 3)
+            Console.WriteLine("           ________\r\n          |       \r\n          |          \r\n          |        \r\n          |          \r\n          |       \r\n          |        \r\n          |          \r\n          |\r\n        __|__________");
+        if (error == 4)
+            Console.WriteLine("           ________\r\n          |      |\r\n          |     ( )\r\n          |     _|_\r\n          |    / | \\\r\n          |      |\r\n          |     / \\\r\n          |    /   \\\r\n          |\r\n        __|__________");
+    }
+}
+       /* ________
+          |      |
+          |     ( )
+          |     _|_
+          |    / | \
+          |      |
+          |     / \
+          |    /   \
+          |
+        __|__________);*/        
